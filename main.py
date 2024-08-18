2fa8adbb1cd9b2aa7af3b698abdb4031e4f9bf03
Barani <89148151+Baranidharan333@users.noreply.github.com>
2024-08-18 21:40:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,2 @@
 print("BARANI_K")
-"""
-import machine
-import time
-led = machine.Pin(2, machine.Pin.OUT)
-while True:
-     led.value(1)
-     time.sleep(1)
-     led.value(0)
-     time.sleep(1)
-"""
+
