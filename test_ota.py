b136300ebc055e49b4ae75d5442c99c03cc0a207
Barani <89148151+Baranidharan333@users.noreply.github.com>
2024-08-19 22:37:52 +0530

    Update test_ota.py

--- test_ota.py
+++ test_ota.py
@@ -1,1 +1,13 @@
-print("BARANI_IS_KING_MISTRY")
+print("BARANI_OTA")
+
+from machine import Pin
+from time import sleep
+
+led = Pin(2, Pin.OUT)
+def ota():
+    while True:
+        led.value(1)
+        sleep(1)
+        led.value(0)
+        sleep(1)
+        print("BARANI")
