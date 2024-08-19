1fbf8fa480f8f5c57d4305f7cea6bf3aee064a8f
Barani <89148151+Baranidharan333@users.noreply.github.com>
2024-08-19 22:41:00 +0530

    Update test_ota.py

--- test_ota.py
+++ test_ota.py
@@ -7,7 +7,7 @@ led = Pin(2, Pin.OUT)
 def ota():
     while True:
         led.value(1)
-        sleep(1)
+        sleep(5)
         led.value(0)
-        sleep(1)
+        sleep(5)
         print("BARANI")
